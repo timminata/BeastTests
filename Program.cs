ae3e34b1f8d82da4f00616da743ed003943a7aa2
Tim <timt@whereismytransport.com>
2018-05-22 15:45:18 +0200

    Testing some stuff

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using GTFS;
+using GTFS.Entities;
+using System;
+
+namespace BeastTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var feed = new GTFS.GTFSFeed();
+            feed.Agencies.Add(new Agency());
+
+            Console.WriteLine(feed.Agencies.Count);
+        }
+    }
+}
